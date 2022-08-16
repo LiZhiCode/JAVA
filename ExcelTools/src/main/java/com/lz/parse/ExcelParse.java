1db783e85eb4c5343cd3c85154ac89608377d2a8
li-zhi-qi <li.zhi.qi@hp.com>
2022-08-16 23:35:00 +0800

    java poi

--- ExcelTools/src/main/java/com/lz/parse/ExcelParse.java
+++ ExcelTools/src/main/java/com/lz/parse/ExcelParse.java
@@ -4,48 +4,55 @@ import com.alibaba.excel.EasyExcel;
 import com.alibaba.excel.context.AnalysisContext;
 import com.alibaba.excel.event.AnalysisEventListener;
 
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 /**
  * @author qiliz
  */
 public class ExcelParse {
+    static final List<String> CPK_LIST = new LinkedList<>();
+    static final List<String> CP_LIST = new LinkedList<>();
+    static String cpkName = "Est. Cpk";
+    static String cpName = "Est. Cp";
     public static void main(String[] args) {
         final List<Map<Integer, String>> excelDataList = new LinkedList<>();
-        final List<Map<Integer, String>> dimensionDataList = new LinkedList<>();
-        final List<Map<Integer, String>> cpDataList = new LinkedList<>();
-        final List<Map<Integer, String>> cpkDataList = new LinkedList<>();
-        int count = 0;
+        final List<Map<Integer, String>> estCpkDataList = new LinkedList<>();
+        final List<Map<Integer, String>> estCpDataList = new LinkedList<>();
         EasyExcel.read("E:\\_Info\\Work\\HP\\upload\\7QR88-40126_13Jun2022_CPK.xlsm")
                 .sheet("P1")
                 .registerReadListener(new AnalysisEventListener<Map<Integer, String>>() {
                     @Override
                     public void invoke(Map<Integer, String> integerStringMap, AnalysisContext analysisContext) {
-                        if ("Dimension #".equals(integerStringMap.get(0))) {
-                            dimensionDataList.add(integerStringMap);
+                        if (cpkName.equals(integerStringMap.get(0))) {
+                            estCpkDataList.add(integerStringMap);
                         }
-                        excelDataList.add(integerStringMap);
-                        System.out.println(integerStringMap);
+                        if (cpName.equals(integerStringMap.get(0))) {
+                            estCpDataList.add(integerStringMap);
+                        }
                     }
                     @Override
                     public void doAfterAllAnalysed(AnalysisContext analysisContext) {
-                        System.out.println("数据读取完毕");
+                        // System.out.println("数据读取完毕");
                     }
                 })
                 .doRead();
-//        for (Map<Integer, String> integerStringMap : excelDataList) {
-//            Set<Integer> keySet = integerStringMap.keySet();
-//            count++;
-//            for (Integer key : keySet) {
-//                System.out.print(key + ":" + integerStringMap.get(key) + ", ");
-//            }
-//            System.out.println(count);
-//            System.out.println("");
-//        }
-//        System.out.println(excelDataList);
-        System.out.println(dimensionDataList);
+        getDataProcessing(estCpkDataList,"cpk");
+        getDataProcessing(estCpDataList, "cp");
+        System.out.println(CPK_LIST);
+        System.out.println(CP_LIST);
+    }
+    public static <name> void getDataProcessing(List<Map<Integer, String>> estCpkDataList, String name) {
+        for (Map<Integer, String> integerStringMap : estCpkDataList) {
+            for (Integer key : integerStringMap.keySet()) {
+                if ( key > 0 && key < 16) {
+                    if (Objects.equals(name, "cpk")) {
+                        CPK_LIST.add(integerStringMap.get(key));
+                    } else {
+                        CP_LIST.add(integerStringMap.get(key));
+                    }
+                }
+            }
+        }
+
     }
 }
--- ExcelTools/src/main/java/com/lz/parse/ExcelPoi.java
+++ ExcelTools/src/main/java/com/lz/parse/ExcelPoi.java
@@ -1,16 +1,13 @@
 package com.lz.parse;
 
 import org.apache.poi.ss.usermodel.*;
-import org.apache.poi.xssf.usermodel.XSSFColor;
-import org.apache.poi.xssf.usermodel.XSSFFont;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Iterator;
+import java.util.List;
 
 /**
  * @author qiliz
@@ -23,59 +20,65 @@ public class ExcelPoi {
         FileInputStream fileInputStream = new FileInputStream(filePath);
         XSSFWorkbook workbook = new XSSFWorkbook(fileInputStream);
         Sheet sheet = workbook.getSheet("P1");
-        ArrayList<Object> arrayList = new ArrayList<>();
+        List<Object> estCpkList = new ArrayList<>();
         Iterator<Row> rows = sheet.rowIterator();
         System.out.println(sheet.getRow(7).getPhysicalNumberOfCells());
 
+        int rowCount = sheet.getRow(7).getPhysicalNumberOfCells();
         Row row;
         Cell cell;
         while(rows.hasNext()){
             row = rows.next();
             //获取单元格
             Iterator<Cell> cells =row.cellIterator();
-            while (cells.hasNext()){
-                cell = cells.next();
-                CellType cellType = cell.getCellType();
+            for (int rowNum = 0; rowNum < rowCount; rowNum++) {
+                if (cells.hasNext()) {
+                    cell = cells.next();
+                    CellType cellType = cell.getCellType();
 
-                switch (cellType) {
-                    //如果该excel是公式
-                    case FORMULA:
-                        try {
-                            cellValue = String.valueOf(cell.getNumericCellValue());
-                        } catch (IllegalStateException e) {
-                            if (e.getMessage().contains("from a STRING cell")) {
-                                cellValue = String.valueOf(cell.getStringCellValue());
-                            } else if (e.getMessage().contains("from a ERROR formula cell")) {
-                                cellValue = String.valueOf(cell.getErrorCellValue());
+                    switch (cellType) {
+                        //如果该excel是公式
+                        case FORMULA:
+                            try {
+                                cellValue = String.valueOf(cell.getNumericCellValue());
+                            } catch (IllegalStateException e) {
+                                if (e.getMessage().contains("from a STRING cell")) {
+                                    cellValue = String.valueOf(cell.getStringCellValue());
+                                } else if (e.getMessage().contains("from a ERROR formula cell")) {
+                                    cellValue = String.valueOf(cell.getErrorCellValue());
+                                }
                             }
-                        }
-                        break;
-                    //如果该excel是字符串
-                    case STRING:
-                        cellValue = String.valueOf(cell);
-                        break;
-                    //如果是double型
-                    case NUMERIC:
-                        cellValue = String.valueOf(cell.getNumericCellValue());
-                        break;
-                    //如果是空格
-                    case BLANK:
-                        break;
-                    //如果是布尔类型
-                    case BOOLEAN:
-                        cellValue = String.valueOf(cell.getBooleanCellValue());
-                        break;
-                    case _NONE:
-                        cellValue = "EXCEPTION:NONE";
-                        break;
-                    case ERROR:
-                        cellValue = "EXCEPTION:ERROR";
-                        break;
-                    default: cellValue = " ";
+                            break;
+                        //如果该excel是字符串
+                        case STRING:
+                            cellValue = String.valueOf(cell);
+                            break;
+                        //如果是double型
+                        case NUMERIC:
+                            cellValue = String.valueOf(cell.getNumericCellValue());
+                            break;
+                        //如果是空格
+                        case BLANK:
+                            break;
+                        //如果是布尔类型
+                        case BOOLEAN:
+                            cellValue = String.valueOf(cell.getBooleanCellValue());
+                            break;
+                        case _NONE:
+                            cellValue = "EXCEPTION:NONE";
+                            break;
+                        case ERROR:
+                            cellValue = "EXCEPTION:ERROR";
+                            break;
+                        default: cellValue = " ";
+                    }
+                    if (cellValue.contains("Est. Cpk")) {
+                        estCpkList.add(cellValue);
+                    }
+//                    System.out.print(cellValue + ", ");
                 }
-                System.out.print(cellValue + ", ");
             }
-            System.out.println();
+            System.out.println(estCpkList);
         }
 
 
